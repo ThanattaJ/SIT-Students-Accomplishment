1f6250721d7cbf20d82a1de22dd075fee36efdcf
Sophon <sophonjampasornklin@gmail.com>
2019-03-23 23:02:38 +0700

    Header
    [Logo]

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
+    <div id="head_bg"></div>
+    <div id="box-inside">
+      <nav class="navbar" role="navigation" aria-label="main navigation" id="nav-top" > 
+        <div class="navbar-brand">
+          <a class="navbar-start" >
+            <div id="logo">
+              <img src="./assets/Logo_Pro.png">
+            </div>
+          </a>
+          <a class="button" id="login" >
+            Login
+          </a>
+        </div>
+      </nav>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
 
@@ -15,8 +30,31 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #553fa7;
-  margin-top: 60px;
 }
+#logo{
+  width: 260px;
+}
+#box-inside{
+   width: 80%;
+  position: relative;
+  margin-left: 10%;
+}
+#head_bg{
+  height: 120px;
+  background-color:#265080;
+}
+#nav-top{
+  height: 120px;
+  background-color:#265080;
+  margin-top: -120px;
+}
+#login{
+  position:absolute;
+  right: 20px;
+  color: aliceblue;
+  font-size: 80%;
+  background: none;
+  margin-top: 30px;
+}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="Home">
+    <div class="header" id="header">
+      <div class="text_header" id="text_header"><br/><br/>
+        <p>Welcome to School of Information Technology </p><br/>
+        <p>@ King Mongkut's University of Technology Thonburi </p>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import './../../node_modules/bulma/css/bulma.css';
+import Multiselect from 'vue-multiselect'
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#header{
+  background: #F8F8F8;
+  height: 100px;
+}
+#text_header{
+  text-align: center;
+  font-size: 10px;
+} 
+</style>
