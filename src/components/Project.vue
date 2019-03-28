8010deb304eef60fd26f541d60d62488387d4379
Sophon <sophonjampasornklin@gmail.com>
2019-03-29 01:57:16 +0700

    [Update]Mock Assignment

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,22 +1,56 @@
 <template>
-  <div>
-      <h1 id="test">Project  {{ msg }} </h1> <br>
-  </div>
+    <div id="Project-bg">
+        <div class="columns">
+            <div class="column is-2">
+                <div class="field">
+                    <div class="control" id="searchbar">
+                        <input class="input" type="text" placeholder="Search Project...">
+                    </div>
+                </div>
+                <aside class="manu">
+                    <ul class="menu-list">
+                        <li><a>All Project</a></li>
+                        <li><a>Assignment Project</a></li>
+                        <li><a>External Project</a></li>
+                    </ul>
+                </aside>
+            </div>
+            <div class="column">
+                <div class="card" id="card">
+                    <div class="card-image">
+                        <figure class="image is-3by2">
+                            <img src="../assets/LUNA.png">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="content">
+                            <p id="h1">SIT Students Accomplishment</p>
+                            <p id="h2">โหลๆเทสๆๆ</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-3by2">
+                            <img src="../assets/plus-symbol.png">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import './css/Project.css'
 export default {
-  name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
 </script>
 
-<style scoped>
-    #test{
-        margin-top: 100px;
-    }
-</style>
+
