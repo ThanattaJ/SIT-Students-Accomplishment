0a95ab2795491010896189ab12f4d4805d4ee6ad
Sophon <sophonjampasornklin@gmail.com>
2019-09-27 20:53:27 +0700

    [edit] name login.vue

--- src/components/auth/login.vue
+++ src/components/auth/login.vue
@@ -1,49 +1,4 @@
 <template>
-<<<<<<< Updated upstream:src/components/login/login_Modal.vue
-  <div id="login">
-    <form @submit.prevent="handleSubmit">
-       <md-card md-with-hover id="flex-container" style="border-radius: 6px;">
-          <div id="sign-up">
-            <form>
-                <md-card-header>
-                   <md-card-header-text>
-                     <div class="md-title" id="title">Sign in</div>
-                   </md-card-header-text>
-                </md-card-header>
-                <div class="grey-text">
-                      <md-card-content>
-                        <md-field>
-                          <label>User Name</label>
-                          <md-textarea v-model="user" md-autogrow></md-textarea>
-                        </md-field>
-                         <div class="md-layout-item">
-                          <md-field>
-                              <label for="movie">selected subfix</label>
-                              <md-select v-model="subfix" name="subfix" id="subfix">
-                              <md-option value="@sit.kmutt.ac.th" selected>@sit.kmutt.ac.th</md-option>
-                              <md-option value="@sit.com">@sit.com</md-option>
-
-                            </md-select>
-                          </md-field>
-                        </div>
-                        <md-field>
-                          <label>Password</label>
-                          <md-input v-model="pass" type="password"></md-input>
-                        </md-field>
-                      </md-card-content>
-                </div>
-                <div class="text-center" id="btm">
-                    <a class="button" id="login-btm" >Log in </a>
-                </div> 
-            </form>
-          </div>
-          <div id="media">
-            <img src="./../../assets/login.jpg" alt="img"/>
-          </div>
-       </md-card>
-    </form>
-  </div>
-=======
 <div id="login">
     <form class="login" @submit.prevent="onLogin">
         <md-card md-with-hover id="flex-container" style="border-radius: 6px;">
@@ -76,7 +31,7 @@
                         </md-card-content>
                     </div>
                     <div class="text-center" id="btm">
-                        <a id="login" type="submit" @click="onLogin(user,userType,pass)">Log in </a>
+                        <a id="login" type="submit" @click="onLogin(username,userType,pass)">Log in </a>
                     </div>
                 </form>
             </div>
@@ -86,49 +41,9 @@
         </md-card>
     </form>
 </div>
->>>>>>> Stashed changes:src/components/auth/login.vue
 </template>
 
 <script>
-<<<<<<< Updated upstream:src/components/login/login_Modal.vue
-import { mapState, mapActions } from 'vuex'
-  export default {
-    name: 'Login',
-    data (){
-      return{
-        user : '',
-        subfix: '',
-        pass: '',
-        submitted: false
-      }
-    },
-    computed: {
-        ...mapState('account', ['status'])
-    },
-    watch: {
-      error (value) {
-        if (value) {
-          this.alert = true
-        }
-      },
-      alert (value) {
-        if (!value) {
-          this.$store.dispatch('setError', false)
-        }
-      }
-    },
-    methods: {
-       ...mapActions('account', ['login', 'logout']),
-       handleSubmit (e) {
-           this.submitted = true;
-           const { username, password } = this;
-           if (username && password) {
-               this.login({ username, password })
-           }
-       }
-    }
-  } 
-=======
 import axios from 'axios';
 import {
     mapState,
@@ -186,7 +101,6 @@ export default {
         }
     }
 }
->>>>>>> Stashed changes:src/components/auth/login.vue
 </script>
 
 <style>
