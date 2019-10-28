472116a4b9a85d19340b8ff34eb651897398fd43
nattanat <59130500024@st.sit.kmutt.ac.th>
2019-10-28 14:02:21 +0700

    [Update] Search (visitor)
    - filter by year
    - search by project name
    - filter by achievement

--- src/components/store/projectStore.js
+++ src/components/store/projectStore.js
@@ -2,16 +2,23 @@ import axios from "axios";
 
 export const projectStore = {
     state: {
-        data: {}
+        data: {},
+        years: []
+
     },
     actions: {
-        LOAD_ALL_PROJECT_VISITORVIEW: async function ({ commit, rootState }, {type, year}) {
+        LOAD_ALL_PROJECT_VISITORVIEW: async function ({ commit, state, rootState }, {type, year}) {
             const URL = rootState.pathStore.pathName+"/projects/"+type+"?year="+year
             console.log("URL : "+URL)
             const { data } = await axios.get(
                 URL
             );
-            commit('SET_ALL_PROJECT_VISITORVIEW', data)
+            console.log("====== LOAD_ALL_PROJECT_VISITORVIEW ======")
+            console.log("",data.projects)
+            commit('SET_ALL_PROJECT_VISITORVIEW', data.projects)
+            if(state.years.length == 0) {
+                commit('SET_YEARS', data.years) 
+             }
         },
         LOAD_PROJECT_BY_SEARCH: async function ({ commit, rootState }, {searchText, searchBy}) {
             const URL = rootState.pathStore.pathName+"/projects/search?by="+searchBy+"&search="+searchText
@@ -20,17 +27,22 @@ export const projectStore = {
                 URL
             );
             commit('SET_ALL_PROJECT_VISITORVIEW', data)
-            commit('SET_ALL_PROJECT_VISITORVIEW', data)
         },
     },
     mutations: {
         SET_ALL_PROJECT_VISITORVIEW: function (state, projects) {
             state.data = projects
+        },
+        SET_YEARS: function (state, years) {
+            state.years = years
         }
     },
     getters: {
         GET_ALL_PROJECT_VISITORVIEW: function (state) {
             return state.data
+        },
+        GET_YEARS: function (state) {
+            return state.years
         },
     }
 }
--- src/components/visitor/allProjectList.vue
+++ src/components/visitor/allProjectList.vue
@@ -41,9 +41,26 @@
     <div id="bodyBg">
         <div class="field has-addons">
             <p class="control">
-                <input id="searchText" class="input" type="text" placeholder="Looking for?" style="font-size: 16px !important;margin-top: 0px !important;border-bottom: 1px solid #DBDBDB !important;">
+                <span class="select">
+                    <select id="selectYear" @change="selectYear">
+                        <option :value="year.year" class="navCanClick" v-for="(year,index) in years" v-bind:key="index">
+                            {{year.year}}
+                        </option>
+                    </select>
+                </span>
             </p>
-            <div class="control">
+            <p class="control">
+                <input id="searchText" class="input" type="text" v-model="search" placeholder="Looking for?" style="font-size: 16px !important;margin-top: 0px !important;border-bottom: 1px solid #DBDBDB !important;">
+            </p>
+            <p class="control">
+                <span class="select">
+                    <select id="haveAchievement" v-model="selectAchievement">
+                        <option value="false">All projects</option>
+                        <option value="true">Achievement projects</option>
+                    </select>
+                </span>
+            </p>
+            <!-- <div class="control">
                 <div class="dropdown is-hoverable">
                     <div class="dropdown-trigger">
                         <button class="button filter" aria-haspopup="true" aria-controls="dropdown-menu4">
@@ -54,66 +71,58 @@
                         <div class="dropdown-content">
                             <div class="dropdown-item">
                                 <p class="menu-label">
-                                    All prejects
+                                    All projects
                                 </p>
                                 <a href="#" class="dropdown-item" style="color:black !important">
-                                    <input type="checkbox">have achievement
+                                    <input type="checkbox" @change="selectHaveAchievement">have achievement
                                 </a>
                                 <a href="#" class="dropdown-item" style="color:black !important">
                                     <input type="checkbox">have assignment
                                 </a>
-                                <p class="menu-label">
-                                    Year
-                                </p>
-                                <div class="dropdown-item select">
-                                    <select id="proficiency_name" class="selectInGenResume" required>
-                                        <option selected>Present</option>
-                                        <option>2018</option>
-                                    </select>
-                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <p class="control" @click="search">
+            </div> -->
+            <!-- <p class="control" @click="search">
                 <a class="button" style="color: #265080 !important;">
                     <i class="la la-search"></i>
                 </a>
-            </p>
+            </p> -->
         </div>
     </div>
     <!-- All projects -->
     <div id="bodyBg">
         <div class="columns is-multiline">
-            <div class="column is-one-quarter" v-for="(allProject,index) in allProjectPresent.projects" v-bind:key="index">
-                <router-link :to="`/ProjectDetail/${allProject.id}`">
+            <div class="column is-one-quarter" v-for="(project,index) in allProjects" v-bind:key="index">
+                <router-link :to="`/ProjectDetail/${project.id}`">
                     <div class="card projectCard content_img">
-                        <div class="card-image" v-if="allProject.cover_path != null">
+                        <div class="card-image" v-if="project.cover_path != null">
                             <figure class="image is-4by2">
-                                <img src="./../../assets/gold-medal.png" width="17px" v-if="allProject.achievement" style="z-index:2;position:absolute;width:40px;">
-                                <img :src="allProject.cover_path" alt="Placeholder image" style="height: 156.22px !important;border-radius: 5px;">
+                                <img src="./../../assets/gold-medal.png" width="17px" v-if="project.achievement" style="z-index:2;position:absolute;width:40px;">
+                                <img :src="project.cover_path" alt="Placeholder image" style="height: 156.22px !important;border-radius: 5px;">
                                 <div class="img-text">
                                     <img src="./../../assets/visibility-button.png" style="width:15px;display: inline;">
-                                    <span class="countText">{{allProject.count_viewer}} </span>
+                                    <span class="countText">{{project.count_viewer}} </span>
                                     <img src="./../../assets/clap-hands.png" style="width:15px;display: inline;">
-                                    <span class="countText">{{allProject.count_clap}} </span>
+                                    <span class="countText">{{project.count_clap}} </span>
                                 </div>
                             </figure>
                         </div>
                         <div class="card-image" v-else>
                             <figure class="image is-4by2">
+                                <img src="./../../assets/gold-medal.png" width="17px" v-if="project.achievement" style="z-index:2;position:absolute;width:40px;">
                                 <img src="./../../assets/noCoverImg.png" style="height: 156.22px !important;border-radius: 5px;">
                                 <div class="img-text">
                                     <img src="./../../assets/visibility-button.png" style="width:15px;display: inline;">
-                                    <span class="countText">{{allProject.count_viewer}} </span>
+                                    <span class="countText">{{project.count_viewer}} </span>
                                     <img src="./../../assets/clap-hands.png" style="width:15px;display: inline;">
-                                    <span class="countText">{{allProject.count_clap}} </span>
+                                    <span class="countText">{{project.count_clap}} </span>
                                 </div>
                             </figure>
                         </div>
                         <div class="card-content projectInfo">
-                            <p class="projectName" style="height: 22px;overflow: hidden;">{{allProject.project_name_en}}</p>
+                            <p class="projectName" style="height: 22px;overflow: hidden;">{{project.project_name_en}}</p>
                         </div>
                     </div>
                 </router-link>
@@ -135,13 +144,48 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            hoverCardOrNot: false
+            hoverCardOrNot: false,
+            selectAchievement: false,
+            search: ""
         }
     },
     computed: {
         ...mapGetters({
-            allProjectPresent: 'GET_ALL_PROJECT_VISITORVIEW'
-        })
+            allProjectPresent: 'GET_ALL_PROJECT_VISITORVIEW',
+            years: 'GET_YEARS'
+        }),
+
+        allProjects() {
+            if (this.search != "" && this.selectAchievement == 'true') {
+                console.log("มี text & achievement")
+                return this.allProjectPresent.filter(
+                    items =>
+                    items.project_name_en.toLowerCase().includes(this.search.toLowerCase()) &&
+                    items.achievement
+                )
+            } else if (this.search == "" && this.selectAchievement == 'true') {
+                console.log("ไม่มี text แต่มี achievement")
+                return this.allProjectPresent.filter(
+                    items =>
+                    items.achievement
+                )
+            } else if (this.search != "" && this.selectAchievement == 'false') {
+                console.log("มี text แต่ไม่มี achievement")
+                return this.allProjectPresent.filter(
+                    items =>
+                    items.project_name_en.toLowerCase().includes(this.search.toLowerCase())
+                )
+            } else {
+                console.log("ไม่ filter อะไรเลย")
+                return this.allProjectPresent
+            }
+        }
+    },
+    watch: {
+        selectAchievement: function (val) {
+            this.selectAchievement = val
+            console.log("selectAchievement : changed >> ", this.selectAchievement + " | " + typeof (this.selectAchievement))
+        }
     },
     mounted() {
         this.LOAD_ALL_PROJECT_VISITORVIEW({
@@ -151,28 +195,36 @@ export default {
     },
     methods: {
         ...mapActions(['LOAD_ALL_PROJECT_VISITORVIEW', 'LOAD_PROJECT_BY_SEARCH']),
-        search() {
-            var typeOfSearch = document.getElementById('typeOfSearch').value
-            var searchText = document.getElementById('searchText').value
-            var searchBy = document.getElementById('searchBy').value
-            var year = document.getElementById('year').value
+        // search() {
+        //     var typeOfSearch = document.getElementById('typeOfSearch').value
+        //     var searchText = document.getElementById('searchText').value
+        //     var searchBy = document.getElementById('searchBy').value
+        //     var year = document.getElementById('year').value
 
-            console.log("typeOfSearch : " + document.getElementById('typeOfSearch').value)
-            console.log("searchText : " + document.getElementById('searchText').value)
-            console.log("searchBy : " + document.getElementById('searchBy').value)
-            console.log("year : " + document.getElementById('year').value)
+        //     console.log("typeOfSearch : " + document.getElementById('typeOfSearch').value)
+        //     console.log("searchText : " + document.getElementById('searchText').value)
+        //     console.log("searchBy : " + document.getElementById('searchBy').value)
+        //     console.log("year : " + document.getElementById('year').value)
 
-            if (searchText == "") {
-                this.LOAD_ALL_PROJECT_VISITORVIEW({
-                    type: typeOfSearch,
-                    year: year
-                })
-            } else {
-                this.LOAD_PROJECT_BY_SEARCH({
-                    searchText: searchText,
-                    searchBy: searchBy
-                })
-            }
+        //     if (searchText == "") {
+        //         this.LOAD_ALL_PROJECT_VISITORVIEW({
+        //             type: typeOfSearch,
+        //             year: year
+        //         })
+        //     } else {
+        //         this.LOAD_PROJECT_BY_SEARCH({
+        //             searchText: searchText,
+        //             searchBy: searchBy
+        //         })
+        //     }
+        // },
+        selectYear() {
+            var selectYear = document.getElementById("selectYear").value
+            console.log("selectYear : " + selectYear)
+            this.LOAD_ALL_PROJECT_VISITORVIEW({
+                type: "all",
+                year: selectYear
+            })
         }
     }
 }
