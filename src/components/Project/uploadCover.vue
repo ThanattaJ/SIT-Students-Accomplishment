b7bd583ff941bcf8c88425a2fc6cec5fa8f831b5
Sophon <sophonjampasornklin@gmail.com>
2019-10-30 23:03:04 +0700

    [edit] upload cover alert check size

--- src/components/Project/uploadCover.vue
+++ src/components/Project/uploadCover.vue
@@ -12,29 +12,31 @@
                                 </p>
                             </div>
                             <div class="modal" v-bind:class="{'is-active':isActive}" id="modalCover">
-                                <!-- <header class="modal-card-head">
+                                <form @submit="onSubmit">
+                                    <!-- <header class="modal-card-head">
                                         <p class="modal-card-title">Modal title</p>
                                         <button class="delete" aria-label="close"></button>
                                     </header> -->
-                                <div class="modal-background"></div>
-                                <div class="modal-content">
-                                    <div class="box">
-                                        <div class="content has-text-centered">
-                                            <p class="control" />
-                                            <h5>{{messageUpload}}</h5>
-                                            <input type="file" ref="file" @change="selectFile" class="file-input" accept=".jpg, .png, .gif" id="inputCovr" />
-                                            <img v-if="url" :src="url" id="cover"/>
-                                            <span>&nbsp;</span>
-                                            <span v-if="file" class="file-name">{{file.name}}</span>
-                                            <p>{{message}}</p>
-                                            <div class="column" id="update">
-                                                <md-button class="md-raised md-primary" @click="sendFile" @submit="onSubmit">Update</md-button>
-                                                <md-button class="md-accent" @click="close">cancle</md-button>
+                                    <div class="modal-background"></div>
+                                    <div class="modal-content">
+                                        <div class="box">
+                                            <div class="content has-text-centered">
+                                                <p class="control" />
+                                                <h5>{{messageUpload}}</h5>
+                                                <input type="file" ref="file" @change="selectFile" class="file-input" accept=".jpg, .png, .gif" id="inputCovr" />
+                                                <img v-if="url" :src="url" id="cover" />
+                                                <span>&nbsp;</span>
+                                                <span v-if="file" class="file-name">{{file.name}}</span>
+                                                <p>{{message}}</p>
+                                                <div class="column" id="update">
+                                                    <md-button class="md-raised md-primary" @click="sendFile" type="submit">Update</md-button>
+                                                    <md-button class="md-accent" @click="close">cancle</md-button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <!-- <button @click="close" class="modal-close"></button> -->
+                                    <!-- <button @click="close" class="modal-close"></button> -->
+                                </form>
                             </div>
                         </div>
                     </span>
@@ -98,6 +100,7 @@ export default {
                 const {
                     data
                 } = await axios.post('https://www.sit-acc.nruf.in.th/files/image', formData)
+                //https://www.sit-acc.nruf.in.th
 
                 this.message = "File has been uploaded";
                 this.error = false;
@@ -122,7 +125,7 @@ export default {
         },
         close: function () {
             this.isActive = false;
-            this.url =''
+            this.url = ''
         },
         onSubmit(e) {
             const file = this.$refs.file.files[0];
@@ -131,7 +134,7 @@ export default {
                 alert('No file chosen');
                 return;
             }
-            if (file.size > 1024) {
+            if (file.size > 35000000) {
                 e.preventDefault();
                 alert('File too big (> 1MB)');
                 return;
@@ -186,11 +189,11 @@ export default {
 .modal {
     overflow: hidden;
 }
-#cover{
+
+#cover {
     max-width: 300px;
     max-height: 300px;
     margin-right: -120px;
     margin-top: 10px
-
 }
 </style>
