134330f9fb969d2ba767a84019e8d91a9cff7217
nattanat <59130500024@st.sit.kmutt.ac.th>
2019-11-15 23:31:07 +0700

    [Update] no result logo

--- src/components/visitor/facultyProject.vue
+++ src/components/visitor/facultyProject.vue
@@ -14,7 +14,7 @@
         </div>
     </div>
     <div v-if="loading"><img src="../../assets/Rolling-2s-200px.svg" class="center-div"></div>
-    <div v-else>
+    <div v-else-if="courses.length > 0">
         <div id="bodyBg">
             <!-- <search -->
             <!-- <div class="field has-addons">
@@ -50,55 +50,6 @@
                                         <figure class="image is-4by2">
                                             <img src="./../../assets/gold-medal.png" width="17px" v-if="allProject.achievement" style="z-index:2;position:absolute;width:40px;">
                                             <img :src="allProject.cover_path" alt="Placeholder image" style="height: 156.22px !important;border-radius: 5px;">
-                                            <div class="img-text">
-                                                <img src="./../../assets/visibility-button.png" style="width:15px;display: inline;">
-                                                <span class="countText">{{allProject.count_viewer}} </span>
-                                                <img src="./../../assets/clap-hands.png" style="width:15px;display: inline;">
-                                                <span class="countText">{{allProject.count_clap}} </span>
-                                            </div>
-                                        </figure>
-    <div id="bodyBg" v-if="courses.length > 0">
-        <!-- <search -->
-        <!-- <div class="field has-addons">
-            <p class="control">
-                <input id="searchText" class="input" type="text" v-model="search" placeholder="Looking for?" style="font-size: 16px !important;margin-top: 0px !important;border-bottom: 1px solid #DBDBDB !important;">
-            </p>
-        </div> -->
-        <!-- All course -->
-        <div class="columns">
-            <div class="column is-3">
-                <aside class="menu navAssignDetail" v-for="(course,index) in courses" v-bind:key="index">
-                    <ul class="menu-list">
-                        <li>
-                            <a :id="'navAssignment navCourse'+index" class="menu-list navTopic navCanClick" @click="nav(index);showAcademicTerm(index)">
-                                <span :id="'courseName'+index" style="color:#4A4A4A">{{course.course_code}}<br>
-                                    {{course.course_name}}</span>
-                            </a>
-                            <ul :id="'academic_term'+index" style="display:none">
-                                <li v-for="(term,index) in course.term" v-bind:key="index" @click="chooseTerm(course.course_id,term.academic_term_id)"><a><span style="color:#265080">{{term.academic_term}}</span></a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </aside>
-            </div>
-            <!-- All projects -->
-            <div class="columns is-multiline">
-                <div class="column is-one-third" v-for="(allProject,index) in allProjects" v-bind:key="index">
-                    <router-link :to="`/ProjectDetail/${allProject.id}`">
-                        <div class="card projectCard content_img">
-                            <div class="card-image" v-if="allProject.cover_path != null">
-                                <figure class="image is-4by2">
-                                    <img src="./../../assets/gold-medal.png" width="17px" v-if="allProject.achievement" style="z-index:2;position:absolute;width:40px;">
-                                    <img :src="allProject.cover_path" alt="Placeholder image" style="height: 156.22px !important;border-radius: 5px;">
-                                    <div class="img-text">
-                                        <img src="./../../assets/visibility-button.png" style="width:15px;display: inline;">
-                                        <span class="countText">{{allProject.count_viewer}} </span>
-                                        <img src="./../../assets/clap-hands.png" style="width:15px;display: inline;">
-                                        <span class="countText">{{allProject.count_clap}} </span>
-                                    </div>
-                                    <div class="card-image" v-else>
-                                        <figure class="image is-4by2">
-                                            <img src="./../../assets/noCoverImg.png" style="height: 156.22px !important;border-radius: 5px;">
                                             <div class="img-text">
                                                 <img src="./../../assets/visibility-button.png" style="width:15px;display: inline;">
                                                 <span class="countText">{{allProject.count_viewer}} </span>
